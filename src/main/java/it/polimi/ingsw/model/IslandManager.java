c0c86cd8c9f9670ab2bce4b9bc96dbbf823b9bf3
michelelorenzo <michelelorenzo.miranda@mail.polimi.it>
2022-04-13 22:42:01 +0200

    insertion of island class and update of IslandManager

--- src/main/java/it/polimi/ingsw/model/IslandManager.java
+++ src/main/java/it/polimi/ingsw/model/IslandManager.java
@@ -1,4 +1,11 @@
 package it.polimi.ingsw.model;
 
+import java.util.ArrayList;
+
 public class IslandManager {
+    private ArrayList<Island> islandList;
+
+    public ArrayList<Island> getIslandList(){
+        return  this.islandList;
+    }
 }
