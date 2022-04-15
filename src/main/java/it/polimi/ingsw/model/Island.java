585c1eaf787bb48161759418a91a4d35fd998c74
michelelorenzo <michelelorenzo.miranda@mail.polimi.it>
2022-04-15 19:40:14 +0200

    updates of the IslandManager

---
+++ src/main/java/it/polimi/ingsw/model/Island.java
@@ -0,0 +1,94 @@
+package it.polimi.ingsw.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Island {
+    private Map<PawnColor, Integer> islandStudents;
+    private TowerColor towerColor;
+    private int towerNumber;
+    private boolean noEntryTile;
+    private boolean motherNature;
+
+    public void setTowerColor(TowerColor color){
+        this.towerColor = color;
+    }
+    public TowerColor getTowerColor(){
+        return this.towerColor;
+    }
+
+    public void setTowersNumber(int number){
+        this.towerNumber = number;
+    }
+    public int getTowersNumber(){
+        return this.towerNumber;
+    }
+
+    //We don't set/get the whole map but only the integer related to the key color.
+    //The map should be implemented as an HashMap, 'cause Map is an interface.
+    public Map<PawnColor, Integer> getIslandStudents() {
+        return this.islandStudents;
+    }
+
+    //is this really useful?
+    private void setIslandStudents(Map<PawnColor, Integer> islandStudents) {
+        for (Map.Entry<PawnColor, Integer> entry : this.islandStudents.entrySet()) {
+            // using put method to copy one Map to Other
+            islandStudents.put(entry.getKey(),
+                    entry.getValue());
+        }
+    }
+
+    public int getStudentNumber(PawnColor color){
+        return this.islandStudents.get(color);
+    }
+
+    public void addStudent(PawnColor color){
+        this.islandStudents.put(color, getStudentNumber(color)+1);
+    }
+
+    //Come gestiamo il null? Bisogna fare un try/catch?
+    public void removeStudent(PawnColor color){
+        if (getStudentNumber(color) > 0) {
+            this.islandStudents.put(color, getStudentNumber(color)-1);
+        }
+    }
+
+
+    public void setMotherNature(boolean isPresent){
+        this.motherNature = isPresent;
+    }
+    public boolean isMotherNature(){
+        return motherNature;
+    }
+
+    public boolean isNoEntryTile() {
+        return noEntryTile;
+    }
+    public void setNoEntryTile(boolean noEntryTile) {
+        this.noEntryTile = noEntryTile;
+    }
+
+    public int calculatePlayerInfluence(SchoolBoard schoolboard){
+        int totStudents = 0;
+        for (PawnColor color : schoolboard.ProfessorTable.keySet()){
+               if (schoolboard.ProfessorTable.get(color))
+                   totStudents += getStudentNumber(color);
+        }
+        return totStudents;
+    }
+
+    public void assignInfluence(SchoolBoard[] schoolBoardsArray){
+        int maxInfluence = 0;
+        TowerColor influence = null;
+
+        for (SchoolBoard schoolBoard : schoolBoardsArray) {
+            int currentInfluence = calculatePlayerInfluence(schoolBoard);
+            if (currentInfluence > maxInfluence) {
+                maxInfluence = currentInfluence;
+                influence = schoolBoard.TowerColor;
+            }
+        }
+        this.towerColor = influence;
+    }
+}
--- src/main/java/it/polimi/ingsw/model/IslandManager.java
+++ src/main/java/it/polimi/ingsw/model/IslandManager.java
@@ -13,22 +13,41 @@ public class IslandManager {
     //TODO - at the end
     public void setupIslands(){}
 
+    //qui devo capire come fare.. la merge deve sommare gli studenti (ed inoltre dovrebbe tenere conto
+    //graficamente dell'unione delle due isole. Non mi sembra una buona soluzione.
     //Todo
-    public void mergeIslands(Island island, Island island2){}
+    public void checkMergeIslands(int islandPos){
+        if (checkNext(this.islandList.get(islandPos), this.islandList.get(islandPos+1))){
+            mergeIslands(this.islandList.get(islandPos), this.islandList.get(islandPos+1));
+            deleteIsland(this.islandList.get(islandPos+1));
+        }
+        if (checkPrevious(this.islandList.get(islandPos), this.islandList.get(islandPos-1))){
+            mergeIslands(this.islandList.get(islandPos-1), this.islandList.get(islandPos));
+            deleteIsland(this.islandList.get(islandPos));
+        }
+    }
+
+    //todo
+    public void mergeIslands(Island primaryIsland, Island secondaryIsland){
+    }
 
     //todo
     public void deleteIsland(Island island){}
 
-    //todo
-    public boolean checkNext(Island island){
-        return true;
+    public boolean checkNext(Island primaryIsland, Island island2){
+        //if (this.islandList.get(islandPos).getTowerColor().equals(this.islandList.get(islandPos+1).getTowerColor()))
+        return primaryIsland.getTowerColor().equals(island2.getTowerColor());
     }
 
-    //todo
-    public boolean checkPrevious(Island island){ return true; }
+    public boolean checkPrevious(Island primaryIsland, Island island2){
+        //if (this.islandList.get(islandPos).getTowerColor().equals(this.islandList.get(islandPos-1).getTowerColor()))
+        return primaryIsland.getTowerColor().equals(island2.getTowerColor());
+    }
 
-    //todo
-    public void moveMotherNature(int cells){}
+    public void moveMotherNature(int motherPosition, int cells){
+        this.islandList.get(motherPosition).setMotherNature(false);
+        this.islandList.get(motherPosition+cells).setMotherNature(true);
+    }
 
 
 }
