76b45640d9b1f37a044c0e9c408c31940b22817e
michelelorenzo <michelelorenzo.miranda@mail.polimi.it>
2022-04-15 17:34:06 +0200

    completion of island class

--- src/main/java/it/polimi/ingsw/model/IslandManager.java
+++ src/main/java/it/polimi/ingsw/model/IslandManager.java
@@ -8,4 +8,27 @@ public class IslandManager {
     public ArrayList<Island> getIslandList(){
         return  this.islandList;
     }
+
+    //how am I supposed to do this?
+    //TODO - at the end
+    public void setupIslands(){}
+
+    //Todo
+    public void mergeIslands(Island island, Island island2){}
+
+    //todo
+    public void deleteIsland(Island island){}
+
+    //todo
+    public boolean checkNext(Island island){
+        return true;
+    }
+
+    //todo
+    public boolean checkPrevious(Island island){ return true; }
+
+    //todo
+    public void moveMotherNature(int cells){}
+
+
 }
