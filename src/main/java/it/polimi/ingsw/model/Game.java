27e9fc36240a68062240a241e6038352a9ab04de
antoniomercurio <antonio.mercurio@mail.polimi.it>
2022-04-16 12:02:40 +0200

    updates related to some of Game methods

--- src/main/java/it/polimi/ingsw/model/Game.java
+++ src/main/java/it/polimi/ingsw/model/Game.java
@@ -1,4 +1,5 @@
 package it.polimi.ingsw.model;
+import java.lang.reflect.Array;
 import java.util.*;
 public class Game {
     //attributes
@@ -6,17 +7,17 @@ public class Game {
     private Long numberOfPlayers;
     private String gameID;
     private GameMode gameMode;
-    private CloudTile[] cloudTiles;
-    private SchoolBoard[] schoolBoards;
+    private ArrayList<CloudTile> cloudTiles;
+    private ArrayList<SchoolBoard> schoolBoards;
     private IslandManager islandManager;
     protected Map<PawnColor, Integer> bag; //da vedere perché funziona solo se protected
-    private ArrayList<CharacterCard> allCharacterCards; //dare dimensione quando si istanzia - "new ArrayList<charcards>(..dimensione..)"
+    private ArrayList<CharacterCard> allCharacterCards; /*dare dimensione quando si istanzia
+     "new ArrayList<charcards>(..dimensione..)"*/
     private ArrayList<Integer> playerOrder;
     private ArrayList<WizardType> wizards;
     private ArrayList<CharacterCard> chosenCharacterCards;
     private int bank;
 //methods @Setter @Getter
-
     public void setGameMode(GameMode gamemode){
         this.gameMode=gamemode;
     }
@@ -29,11 +30,17 @@ public class Game {
     public String getGameID() {
         return gameID;
     }
-    public CloudTile[] getCloudTiles(){
+    public ArrayList<CloudTile> getCloudTiles() {
         return cloudTiles;
     }
-    public SchoolBoard[] getSchoolBoards(){
+    public void setCloudTiles(ArrayList<CloudTile> cloudTiles) {
+        this.cloudTiles = cloudTiles;
+    }
+    public ArrayList<SchoolBoard> getSchoolBoards() {
         return schoolBoards;
+    }
+    public void setSchoolBoards(ArrayList<SchoolBoard> schoolBoards) {
+        this.schoolBoards = schoolBoards;
     }
     public IslandManager getIslandManager() {
         return islandManager;
@@ -41,7 +48,6 @@ public class Game {
     public Map<PawnColor, Integer> getBag() {
         return bag;
     }
-
     public void setBank(int coins){
         this.bank=coins;
     }
@@ -75,12 +81,6 @@ public class Game {
     public void setIslandManager(IslandManager islandManager) {
         this.islandManager = islandManager;
     }
-    public void setSchoolBoards(SchoolBoard[] schoolBoards) {
-        this.schoolBoards = schoolBoards;
-    }
-    public void setCloudTiles(CloudTile[] cloudTiles) {
-        this.cloudTiles = cloudTiles;
-    }
     public void setChosenCharacterCards(ArrayList<CharacterCard> chosenCharacterCards) {
         this.chosenCharacterCards = chosenCharacterCards;
     }
@@ -88,18 +88,46 @@ public class Game {
         return chosenCharacterCards;
     }
 //
-    public void moveFromBagToCloud(PawnColor color, CloudTile cloud){
-        //TODO
+    public void moveFromBagToCloud(CloudTile cloud){
+        PawnColor rdColor=PawnColor.randomColor();
+        if(this.bag.get(rdColor)==0){
+            while(this.bag.get(rdColor)==0){
+                rdColor=PawnColor.randomColor();
+            }
+        }//controllo che la bag abbia disponibilità di studenti del colore random
+        this.bag.replace(rdColor, this.bag.get(rdColor)-1);
+        cloud.Students.replace(rdColor, cloud.Students.get(rdColor)+1);
+        //manca implementazione della classe cloudtiles -> l'errore dovrebbe risolversi quando la si implementa.
     }
     public void updateProfessor(PawnColor color){
-        //TODO
+        /*int max=-1;
+        for (SchoolBoard p : schoolBoards){
+            controllo max player
+        }*/
+        //TODO update prof
+        /*settare professorTable di tutti i player*/
     }
     public ArrayList<CharacterCard> shuffleCharacterCards(ArrayList<CharacterCard> allCharacterCards) {
         Collections.shuffle(allCharacterCards);
         return allCharacterCards;
     }
     public void SetUpGame(int numberOfPlayers, GameMode gamemode){
-//TODO 1)allocare parti fisiche del gioco; 2) scegliere 1st player; 3)riempire cloud e isole; 4)randomchoosecharactercards;
+        ArrayList<CloudTile> cloudTiles = new ArrayList<CloudTile>(numberOfPlayers);
+        ArrayList<SchoolBoard> schoolBoards= new ArrayList<SchoolBoard>(numberOfPlayers);
+        IslandManager islandManager= new IslandManager();
+        Player firstPlayer = new Player();
+        firstPlayer=SelectFirstPlayer();
+        for(PawnColor color : PawnColor.values()){
+            this.setBag(color, 26);
+        }//setta la bag ai valori di default
+
+        if(gamemode.equals(GameMode.Expert)){
+            initChosenCharacterCards();
+            setBank(20);
+        }
+//TODO 1)allocare parti fisiche del gioco (clouds, islands, bag, schoolboards, decks)
+// 2) scegliere 1st player;
+// 3)riempire cloud e isole;
     }
     public Player SelectFirstPlayer(){
         int min_val=0;
@@ -107,15 +135,20 @@ public class Game {
         Random rd=new Random();
         int randNum=min_val+rd.nextInt((max_val - min_val) + 1);
         return players.get(randNum);
-    }
+    }//genera casualmente il primo giocatore che lancia la carta assistente al primo turno
     public void setBag(PawnColor color, int n){
         this.bag.replace(color,n);
+    //di default setta a 26 ogni color
     }
     public ArrayList<CharacterCard> initChosenCharacterCards() {
         return (ArrayList<CharacterCard>) getAllCharacterCards().subList(0,2);
     }
-
     public ArrayList<CharacterCard> getAllCharacterCards() {
         return shuffleCharacterCards(allCharacterCards);
-    }
+    } /* restituisce tutte le charcards in disordine;
+     quando si chiama la init si pescano le prime 3 in disordine;
+     dopo la init si accede alle chosen tramite la get*/
+
+
+
 }
--- src/main/java/it/polimi/ingsw/model/PawnColor.java
+++ src/main/java/it/polimi/ingsw/model/PawnColor.java
@@ -1,9 +1,15 @@
 package it.polimi.ingsw.model;
+import java.util.Random;
 
 public enum PawnColor {
     yellow,
     blue,
     red,
     green,
-    pink
+    pink;
+
+    public static PawnColor randomColor() {
+        int pick = new Random().nextInt(PawnColor.values().length);
+        return PawnColor.values()[pick];
+    }
 }
