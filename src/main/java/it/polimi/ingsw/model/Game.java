10b8a6d8ea2c99e90e68fbdddfa7c4bbe7284f13
antoniomercurio <antonio.mercurio@mail.polimi.it>
2022-04-23 19:37:38 +0200

    Game class update

--- src/main/java/it/polimi/ingsw/model/Game.java
+++ src/main/java/it/polimi/ingsw/model/Game.java
@@ -1,10 +1,11 @@
 package it.polimi.ingsw.model;
 import java.util.*;
+
 public class Game {
-    //attributes
+//attributes
     private ArrayList<Player> players;
     private int numberOfPlayers;
-    private String gameID;
+    private int gameID;
     private GameMode gameMode;
     private ArrayList<CloudTile> cloudTiles;
     private ArrayList<SchoolBoard> schoolBoards;
@@ -23,10 +24,10 @@ public class Game {
     public GameMode getGameMode() {
         return gameMode;
     }
-    public void setGameID(String gameID){
+    public void setGameID(int gameID){
         this.gameID=gameID;
     }
-    public String getGameID() {
+    public int getGameID() {
         return gameID;
     }
     public ArrayList<CloudTile> getCloudTiles() {
@@ -89,7 +90,7 @@ public class Game {
     public ArrayList<CharacterCard> getChosenCharacterCards() {
         return chosenCharacterCards;
     }
-//
+//other methods
     public void moveFromBagToCloud(CloudTile cloud){
         PawnColor rdColor=PawnColor.randomColor();
         if(this.bag.get(rdColor)==0){
@@ -104,7 +105,7 @@ public class Game {
         int currentMax=0;
         /*
         si potrebbe tenere in game una mappa che tenga traccia del numero di studenti che ha il player
-        che possiede già il prof in modo da effettuare la update solo quando serve (?) piuttosto che farlo
+        che possiede già il prof in modo da effettuare la update solo quando serve piuttosto che farlo
         per ogni studente spostato
         */
         for (SchoolBoard s : schoolBoards){
@@ -124,75 +125,38 @@ public class Game {
     public void SetUpGame(int numberOfPlayers, GameMode gamemode)/*gamemode e noOfPlayers si prendono dal controller?*/{
         this.setNumberOfPlayers(numberOfPlayers);
         this.setGameMode(gamemode);
-        //this.gameID=setGameID(); randomico
-        ArrayList<CloudTile> cloudTiles = new ArrayList<CloudTile>(numberOfPlayers);
-        {
-            for(int i=0;i<numberOfPlayers;i++){
-                cloudTiles.add(new CloudTile());
-            }
-        }
-        {
-            for(CloudTile c : cloudTiles){
-                for(int i=0;i<3;i++){
-                    moveFromBagToCloud(c);
-                }
-                if(numberOfPlayers==3){
-                    moveFromBagToCloud(c);
-                }
-            }
-        }
-        ArrayList<SchoolBoard> schoolBoards= new ArrayList<SchoolBoard>(numberOfPlayers);
-        {
-            for(int i=0;i<numberOfPlayers;i++){
-                schoolBoards.add(new SchoolBoard());
-            }
-        }
-        ArrayList<Island> islands = new ArrayList<Island>(12);
-        {
-            {
-                for(int i=0;i<12;i++){
-                    islands.add(new Island());
-                     if(i!=0&&i!=5){
-                         PawnColor rdColor=PawnColor.randomColor();
-                         this.bag.replace(rdColor, this.bag.get(rdColor)-1);
-                         islands.get(i).addStudent(rdColor);
-                    }//initializes islands with students
-                     else if(i==0){
-                         islands.get(i).setMotherNature(true);
-                     }//initializes mothernature
-                }
-            }
-        }
+        fillBag();
+        /* generatore randomico ID del game (?)
+        Random rn = new Random();
+        int n = 100000 - 0 + 1;
+        int j = rn.nextInt() % n;
+        this.setGameID(j); */
+        ArrayList<CloudTile> cloudTiles = generateCloudTiles(numberOfPlayers);
+        fillCloudTiles(numberOfPlayers, cloudTiles);
+        ArrayList<Island> islands = generateIslands();
         IslandManager islandManager= new IslandManager(islands);
-        ArrayList<Player> players = new ArrayList<Player>(numberOfPlayers);
-        {
-            for(int i=0;i<numberOfPlayers;i++){
-                players.add(new Player());
-            }
-        }
+        ArrayList<Player> players = generatePlayers(numberOfPlayers);
+
+            //todo inizializzare i players
+
+        ArrayList<SchoolBoard> schoolBoards = generateSchoolBoards(numberOfPlayers);
+        initSchoolBoards(schoolBoards);
         Player firstPlayer = SelectFirstPlayer();
-        /*Iterator iterator = players.iterator();
-        while(iterator.next()!=firstPlayer){
-            iterator.next();
-        }*/ //TODO
+        ArrayList<Integer> tmpOrder = new ArrayList<Integer>(numberOfPlayers);
+        for(int i=0;i<numberOfPlayers;i++){
+            tmpOrder.add(firstPlayer.getPlayerNumber()+i);
+        }
+        this.setPlayerOrder(tmpOrder);
         ArrayList<WizardType> wizards = new ArrayList<WizardType>(numberOfPlayers);{
             for(Player p : players) {
                 wizards.add(p.getDeck().getWizard());
             }
         }
-        for(PawnColor color : PawnColor.values()){
-            this.setBag(color, 26);
-        }//setta la bag ai valori di default
         if(gamemode.equals(GameMode.expert)){
             ArrayList<CharacterCard> chosenCharacterCards = initChosenCharacterCards();
             this.setChosenCharacterCards(chosenCharacterCards);
             this.setBank(20);
         }
-/*TODO
- 1)allocare parti fisiche del gioco (cloudsX, islandsX, bagX, schoolboardsX, decks)
- 2) scegliere 1st playerX;
- 3)riempire cloudX e isoleX;
- */
     }
     public Player SelectFirstPlayer(){
         int min_val=0;
@@ -201,7 +165,7 @@ public class Game {
         int randNum=min_val+rd.nextInt((max_val - min_val) + 1);
         return players.get(randNum);
     }//genera casualmente il primo giocatore che lancia la carta assistente al primo turno
-    public void setBag(PawnColor color, int n){
+    public void initBag(PawnColor color, int n){
         this.bag.replace(color,n);
     }
     public ArrayList<CharacterCard> initChosenCharacterCards() {
@@ -213,6 +177,98 @@ public class Game {
      quando si chiama la init si pescano le prime 3 in disordine;
      dopo la init si accede alle chosen tramite la get*/
     public void updatePlayerOrder(){
-        //todo comparator
+        ArrayList<Player> clonePlayerArray = this.players;
+        clonePlayerArray.sort(new Comparator<Player>() {
+            @Override
+            public int compare(Player i1, Player i2) {
+                return i1.getLastAssistantCard().getValue() - i2.getLastAssistantCard().getValue();
+            }
+        });
+        ArrayList<Integer> tmpOrder = new ArrayList<Integer>(numberOfPlayers);
+        for(int i=0;i<numberOfPlayers;i++){
+            tmpOrder.add(clonePlayerArray.get(i).getPlayerNumber());
+        }
+        this.setPlayerOrder(tmpOrder);
+        //dovrebbe andare ma non sono sicurissimo
+    }
+//setupGame extractions
+    private ArrayList<Island> generateIslands() {
+        ArrayList<Island> islands = new ArrayList<Island>(12);
+        {
+            {
+                for(int i=0;i<12;i++){
+                    islands.add(new Island());
+                    if(i!=0&&i!=5){
+                        PawnColor rdColor=PawnColor.randomColor();
+                        this.bag.replace(rdColor, this.bag.get(rdColor)-1);
+                        islands.get(i).addStudent(rdColor);
+                    }//initializes islands with students
+                    else if(i==0){
+                        islands.get(i).setMotherNature(true);
+                    }//initializes mothernature
+                }
+            }
+        }
+        return islands;
+    }
+    private ArrayList<SchoolBoard> generateSchoolBoards(int numberOfPlayers) {
+        ArrayList<SchoolBoard> schoolBoards= new ArrayList<SchoolBoard>(numberOfPlayers);
+        {
+            for(int i = 0; i< numberOfPlayers; i++){
+                schoolBoards.add(new SchoolBoard());
+            }
+        }
+        return schoolBoards;
+    }
+    private void fillCloudTiles(int numberOfPlayers, ArrayList<CloudTile> cloudTiles) {
+        for(CloudTile c : cloudTiles){
+            for(int i=0;i<3;i++){
+                moveFromBagToCloud(c);
+            }
+            if(numberOfPlayers ==3){
+                moveFromBagToCloud(c);
+            }
+        }
+    }
+    private ArrayList<CloudTile> generateCloudTiles(int numberOfPlayers) {
+        ArrayList<CloudTile> cloudTiles = new ArrayList<CloudTile>(numberOfPlayers);
+        {
+            for(int i = 0; i< numberOfPlayers; i++){
+                cloudTiles.add(new CloudTile());
+            }
+        }
+        return cloudTiles;
+    }
+    private void fillBag() {
+        for(PawnColor color : PawnColor.values()){
+            this.initBag(color, 26);
+        }//setta la bag ai valori di default
+    }
+    private ArrayList<Player> generatePlayers(int numberOfPlayers) {
+        ArrayList<Player> players = new ArrayList<Player>(numberOfPlayers);
+        {
+            for(int i = 0; i< numberOfPlayers; i++){
+                players.add(new Player());
+            }
+        }
+        return players;
+    }
+    private void initSchoolBoards(ArrayList<SchoolBoard> schoolBoards) {
+        for(SchoolBoard s: schoolBoards){
+            if(this.numberOfPlayers==2){
+                s.setTowersNumber(8);
+            }
+            if(this.numberOfPlayers==3){
+                s.setTowersNumber(6);
+            }
+            if(this.numberOfPlayers==4){
+                s.setTowersNumber(4);
+            }
+            for(int i=0;i<7;i++){
+                PawnColor rdColor=PawnColor.randomColor();
+                this.bag.replace(rdColor, this.bag.get(rdColor)-1);
+                s.addStudentEntrance(rdColor);
+            }
+        }
     }
 }
