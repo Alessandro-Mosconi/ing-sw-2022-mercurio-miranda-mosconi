25270866f1f4b3447590ddb5022edb8858d65c8d
antonio-mercurio <100923523+antonio-mercurio@users.noreply.github.com>
2022-05-20 13:18:29 +0200

    Controller + model updates

---
+++ src/main/java/it/polimi/ingsw/controller/EndActionState.java
@@ -0,0 +1,33 @@
+package it.polimi.ingsw.controller;
+
+public class EndActionState implements GameControllerState{
+    @Override
+    public void startState(GameController gameController) {
+        //do nothing
+    }
+
+    @Override
+    public void updateNextState(GameController gameController) {
+        if (gameController.getCurrentVirtualView()<gameController.getVirtualViews().size()-1){
+            gameController.setNextState(new MovePawnsState());
+            gameController.setCurrentVirtualView(gameController.getCurrentVirtualView()+1 /*todo % gameController.getVirtualViews().size()*/);
+        }
+        //this branch is when the players have finished their cards or there are no students left in the bag
+        /*else if ((gameController.getGame().getPlayers().get(0).getDeck().getCards().size() < 1) || (calculateBag(gameController) <1)){
+            gameController.setNextState(new EndGameState());
+        }*/
+        else if(gameController.isLastRound()){
+            gameController.setNextState(new EndGameState());
+        }
+        //this branch is for when a new round starts
+        else{
+            gameController.setNextState(new PlanningState());
+            gameController.setCurrentVirtualView(0);
+        }
+    }
+
+    @Override
+    public void endState(GameController gameController) {
+        //do nothing
+    }
+}
