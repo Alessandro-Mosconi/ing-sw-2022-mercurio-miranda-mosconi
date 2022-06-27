df66e5d9edde476ca49bcf8d7a5bb29c3fe0cddf
Michele Lorenzo Miranda <michelelorenzo.miranda@mail.polimi.it>
2022-06-27 14:08:35 +0200

    addition of controllers

---
+++ src/main/java/it/polimi/ingsw/view/controller/CreateSettingController.java
@@ -0,0 +1,44 @@
+package it.polimi.ingsw.view.controller;
+
+import it.polimi.ingsw.model.GameMode;
+import it.polimi.ingsw.view.GUI;
+import it.polimi.ingsw.view.GuiStarter;
+import it.polimi.ingsw.view.View;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class CreateSettingController {
+    public TextField gameMode;
+    public TextField numOfPlayers;
+    public TextField gameID;
+    private Stage primaryStage;
+    private Scene scene;
+    private Parent root;
+
+    private GuiStarter currentApplication;
+
+    public void createGame(ActionEvent actionEvent) throws Exception {
+
+            if ((gameID.getText().length()==0) || (gameMode.getText().length()==0) || (numOfPlayers.getText().length()==0)) {
+                Alert alert = new Alert(Alert.AlertType.ERROR, "Insert parameters!", ButtonType.OK);
+                alert.showAndWait();
+                return;
+            }
+
+        currentApplication = GuiStarter.getCurrentApplication();
+        View view = (GUI) currentApplication.getClient().getView();
+        view.setIdGame(gameID.getText());
+        view.setPlayerNumber(Integer.parseInt(numOfPlayers.getText()));
+        view.setGamemode(GameMode.valueOf(gameMode.getText()));
+
+        primaryStage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        view.prepareMessage();
+    }
+
+}
---
+++ src/main/java/it/polimi/ingsw/view/controller/JoinSettingController.java
@@ -0,0 +1,34 @@
+package it.polimi.ingsw.view.controller;
+
+import it.polimi.ingsw.network.MessageType;
+import it.polimi.ingsw.view.GUI;
+import it.polimi.ingsw.view.GuiStarter;
+import it.polimi.ingsw.view.View;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
+
+public class JoinSettingController {
+    public TextField gameID;
+
+
+    private GuiStarter currentApplication;
+
+
+    public void joinGame(ActionEvent actionEvent) throws Exception {
+
+            if ((gameID.getText().length()==0)) {
+                Alert alert = new Alert(Alert.AlertType.ERROR, "Insert parameters!", ButtonType.OK);
+                alert.showAndWait();
+                return;
+            }
+
+        currentApplication = GuiStarter.getCurrentApplication();
+        View view = (GUI) currentApplication.getClient().getView();
+        view.setIdGame(gameID.getText());
+        view.setMessageType(MessageType.JOIN_MATCH);
+        view.prepareMessage();
+    }
+
+}
